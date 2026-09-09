1cafa44afd720d237fc2a1fcc18798ee1fd3731e
Mahesh Fale <maheshfale431@gmail.com>
2026-09-09 08:34:38 +0530

    Time: 0 ms (100%), Space: 42.7 MB (92.25%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int i = n - 1;
+        while(i >= 0 && s.charAt(i) == ' '){
+            i--;
+        }
+        int len = 0;
+        while(i >= 0 && s.charAt(i) != ' '){
+            len += 1;
+            i--;
+        }
+        return len;
+    }
+}
